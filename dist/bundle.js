dbc3c4aeb24cdc61abe0f6c3c757cc3ce942415c
alirhomadoni <rhomadonia@gmail.com>
2021-11-29 07:21:11 +0700

    udah bener...

--- dist/bundle.js
+++ dist/bundle.js
@@ -185,76 +185,11 @@ process.chdir = function (dir) {
 process.umask = function() { return 0; };
 
 },{}],2:[function(require,module,exports){
-//Fungsi
-
-(function() {
-  // save these original methods before they are overwritten
-  var proto_initIcon = L.Marker.prototype._initIcon;
-  var proto_setPos = L.Marker.prototype._setPos;
-
-  var oldIE = (L.DomUtil.TRANSFORM === 'msTransform');
-
-  L.Marker.addInitHook(function () {
-      var iconOptions = this.options.icon && this.options.icon.options;
-      var iconAnchor = iconOptions && this.options.icon.options.iconAnchor;
-      if (iconAnchor) {
-          iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px');
-      }
-      this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom' ;
-      this.options.rotationAngle = this.options.rotationAngle || 0;
-
-      // Ensure marker keeps rotated during dragging
-      this.on('drag', function(e) { e.target._applyRotation(); });
-  });
-
-  L.Marker.include({
-      _initIcon: function() {
-          proto_initIcon.call(this);
-      },
-
-      _setPos: function (pos) {
-          proto_setPos.call(this, pos);
-          this._applyRotation();
-      },
-
-      _applyRotation: function () {
-          if(this.options.rotationAngle) {
-              this._icon.style[L.DomUtil.TRANSFORM+'Origin'] = this.options.rotationOrigin;
-
-              if(oldIE) {
-                  // for IE 9, use the 2D rotation
-                  this._icon.style[L.DomUtil.TRANSFORM] = 'rotate(' + this.options.rotationAngle + 'deg)';
-              } else {
-                  // for modern browsers, prefer the 3D accelerated version
-                  this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationAngle + 'deg)';
-              }
-          }
-      },
-
-      setRotationAngle: function(angle) {
-          this.options.rotationAngle = angle;
-          this.update();
-          return this;
-      },
-
-      setRotationOrigin: function(origin) {
-          this.options.rotationOrigin = origin;
-          this.update();
-          return this;
-      }
-  });
-})();
-
-
-//Batas
-
-
-
-
+var pusatSiteRadar = [-6.892477,107.579566]
 var map = L.map('map',{
   minZoom: 2,
   maxZoom: 20
-}).setView([-6.892477,107.579566],8);
+}).setView(pusatSiteRadar,8);
 L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=M5m4NpxeLlROSyodQi1E',{
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     crossOrigin: true,
@@ -270,10 +205,11 @@ var airplaneIcon = L.icon({
     iconUrl : 'img/airplane-4-64.png',
     iconSize :[30,30]
 })
-var markerRadar=L.marker([-6.892477,107.579566],{icon : radarIcon}).addTo(map);
+var markerRadar=L.marker(pusatSiteRadar,{icon : radarIcon}).addTo(map);
+markerRadar.bindTooltip("Site Radar Bandung");
 
 
-var circle = L.circle([-6.892477,107.579566],{
+var circle = L.circle(pusatSiteRadar,{
     color : 'red',
     radius: 463000,
     fillColor :'#f03',
